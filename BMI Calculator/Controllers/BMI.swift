c14733098be192eacb41b6b32950f50e8ca35bbe
Francisco Amuchastegui <franamuchastegui@gmail.com>
2023-09-23 21:25:32 -0300

    first commit

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,9 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by francisco amuchastegui on 23/09/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by francisco amuchastegui on 23/09/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
